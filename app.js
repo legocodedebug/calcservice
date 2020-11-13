6ca3cc44a32424a86bebd14c293a486b0f536537
legocodedebug <legocodedebug@gmail.com>
2020-11-13 17:29:07 +0300

    Add Simple Express

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const rookout = require('rookout');
+rookout.start({
+    token: '23064ede612d803b2c15d88723b1fff6fc927dce22d8efbf878a322625927641',
+    labels: {
+        env: 'dev'
+    }
+})
 const express = require('express')
 const app = express()
 const port = 3000
+let counter=0;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    counter++;
+    res.send(`Hello World! ${counter}`)
 })
 
 app.listen(port, () => {
+ 
     console.log(`Example app listening at http://localhost:${port}`)
 })
