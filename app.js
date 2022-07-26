001c10f6f0a70b09d0996df982dd6c603677e081
onurdayibasi <odayibasi@gmail.com>
2022-07-26 16:24:09 +0300

    JestTest Updated

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-const myArgs = process.argv.slice(2);
-const myPort = myArgs[0] ? myArgs[0] : 3000;
-
-const express = require("express");
-const app = express();
-const port = myPort;
-let counter = 0;
-
-app.get("/", (req, res) => {
-  counter++;
-  res.send(`Hello World! ${counter}`);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-function sayHelloA() {
-  let counterA = counter++;
-  console.log(`HelloA ${counterA}`);
-}
-
-function sayHelloB() {
-  let counterB = counter + 2;
-  console.log(`HelloB ${counterB}`);
-}
-
-// setInterval(sayHelloA, 5000);
-// setInterval(sayHelloB, 10000);
-
 function sum(a, b) {
   return a + b;
 }
