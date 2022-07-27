40000684ab2264819eec7f02fc8b96cc10b57480
onurdayibasi <odayibasi@gmail.com>
2022-07-27 12:53:54 +0300

    Sub Function added

--- app.test.js
+++ app.test.js
@@ -1,5 +1,9 @@
-const sum = require("./app.js");
+const { sum, sub } = require("./app.js");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("adds 3 - 1 to equal 2", () => {
+  expect(sub(3, 1)).toBe(2);
 });
