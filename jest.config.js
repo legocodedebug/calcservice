98f08f6a8c90188f6713704081aa76b02c5592be
onurdayibasi <odayibasi@gmail.com>
2022-08-01 10:24:38 +0300

    Updated with coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
-process.env["JEST_JUNIT_OUTPUT_DIR"] = "./report";
+// jest.config.js
+module.exports = {
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageReporters: ["text", "cobertura"],
 
-const config = {
-  // reporters: [ "default", "jest-junit" ],
+  // jest.config.js
+
+  reporters: ["default", "jest-junit"],
+
+  // where to put the code coverage reports.
+  coverageDirectory: "./coverage",
+
+  coveragePathIgnorePatterns: ["./jest"],
+
+  // A path to a module which exports an async function that is triggered once before all test suites
 };
-
-module.exports = config;
