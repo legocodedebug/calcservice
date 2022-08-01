ae670418c50ae97e2d6bfa6ec28e3c49629c6d40
onurdayibasi <odayibasi@gmail.com>
2022-08-01 11:45:10 +0300

    MultipleAndDiv added

--- app.test.js
+++ app.test.js
@@ -1,9 +1,13 @@
-const { sum, sub } = require("./app.js");
+const { sum, sub, mul } = require("./app.js");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("adds 3 - 1 to equal 2", () => {
+test("sub 3 - 1 to equal 2", () => {
   expect(sub(3, 1)).toBe(2);
+});
+
+test("mul 3 x 2 to equal 6", () => {
+  expect(mul(3, 2)).toBe(6);
 });
